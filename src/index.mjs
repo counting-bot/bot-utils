f21769b5bbb58647ac1a4fcc6354983ffcc21bdc
numselli <58607232+numselli@users.noreply.github.com>
2022-02-09 11:08:04 -0800

    rename imports

--- src/index.mjs
+++ src/index.mjs
@@ -1,8 +1,8 @@
-import {default as removeFormattingInport} from './removeFormatting.mjs'
+import removeFormattingInport from './removeFormatting.mjs'
 export const removeFormatting = removeFormattingInport;
 
-import {default as cpuLoad} from './cpuLoad.mjs'
+import cpuLoad from './cpuLoad.mjs'
 export const avgCPUload = cpuLoad;
 
-import {default as formatTimeImport} from './formatTime.mjs'
+import formatTimeImport from './formatTime.mjs'
 export const formatTime = formatTimeImport;
