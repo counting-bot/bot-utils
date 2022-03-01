35e825e6585bd99b390f241c265e6285a2365e7a
numselli <58607232+numselli@users.noreply.github.com>
2022-03-01 11:11:39 -0800

    fix duplicate parentheses

--- src/formatNumber.mjs
+++ src/formatNumber.mjs
@@ -1,5 +1,5 @@
 export default (number, commaSeparator, trim) => {
     const srtNumber = number.toString();
-    const trimedNumber = trim ? ((srtNumber.includes(".") ? `${srtNumber.split(".")[0]}.${srtNumber.split(".")[1].slice(0, 2)}` : srtNumber)) : srtNumber
+    const trimedNumber = trim ? (srtNumber.includes(".") ? `${srtNumber.split(".")[0]}.${srtNumber.split(".")[1].slice(0, 2)}` : srtNumber) : srtNumber
     return commaSeparator ? `${trimedNumber.split(".")[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",")}${srtNumber.includes(".")?".":""}${trimedNumber.split(".")[1]??""}` : trimedNumber
 };
