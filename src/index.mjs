b7abf642382e207baa45b1c6cb927631d015c47c
numselli <58607232+numselli@users.noreply.github.com>
2022-03-28 15:25:50 -0700

    reorganize

--- src/index.mjs
+++ src/index.mjs
@@ -1,8 +1,8 @@
-export {defualt as removeFormatting} from './removeFormatting.mjs'
-export {defualt as avgCPUload} from './cpuLoad.mjs';
-export {defualt as formatTime} from './formatTime.mjs';
-export {defualt as formatNumber} from './formatNumber.mjs';
-export {defualt as formatScoreString} from './scoreFormatting.mjs';
-export {defualt as calculateAccuracy} from './calculateAccuracy.mjs';
-export {defualt as postBotStats} from './postBotStats.mjs';
-export {defualt as erisComponentsCollector} from './erisComponentsCollector.mjs'
+export { defualt as removeFormatting } from './utils/removeFormatting.mjs'
+export { defualt as avgCPUload } from './utils/cpuLoad.mjs';
+export { defualt as formatTime } from './utils/formatTime.mjs';
+export { defualt as formatNumber } from './utils/formatNumber.mjs';
+export { defualt as formatScoreString } from './utils/scoreFormatting.mjs';
+export { defualt as calculateAccuracy } from './utils/utils/calculateAccuracy.mjs';
+export { defualt as postBotStats } from './utils/postBotStats.mjs';
+export { defualt as erisComponentsCollector } from './utils/erisComponentsCollector.mjs'
