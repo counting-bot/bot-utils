06b3c7c251cdd20a06ceff71a0fb0a0b0143b8c4
numselli <58607232+numselli@users.noreply.github.com>
2022-03-28 15:54:27 -0700

    fix package

--- src/index.mjs
+++ src/index.mjs
@@ -3,6 +3,6 @@ export { default as avgCPUload } from './utils/cpuLoad.mjs';
 export { default as formatTime } from './utils/formatTime.mjs';
 export { default as formatNumber } from './utils/formatNumber.mjs';
 export { default as formatScoreString } from './utils/scoreFormatting.mjs';
-export { default as calculateAccuracy } from './utils/utils/calculateAccuracy.mjs';
+export { default as calculateAccuracy } from './utils/calculateAccuracy.mjs';
 export { default as postBotStats } from './utils/postBotStats.mjs';
 export { default as erisComponentsCollector } from './utils/erisComponentsCollector.mjs'
