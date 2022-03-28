38a8ffaaf6f91fc20e78536c167daba39542e04b
numselli <58607232+numselli@users.noreply.github.com>
2022-03-28 15:34:55 -0700

    Update index.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -1,8 +1,8 @@
-export { defualt as removeFormatting } from './utils/removeFormatting.mjs'
-export { defualt as avgCPUload } from './utils/cpuLoad.mjs';
-export { defualt as formatTime } from './utils/formatTime.mjs';
-export { defualt as formatNumber } from './utils/formatNumber.mjs';
-export { defualt as formatScoreString } from './utils/scoreFormatting.mjs';
-export { defualt as calculateAccuracy } from './utils/utils/calculateAccuracy.mjs';
-export { defualt as postBotStats } from './utils/postBotStats.mjs';
-export { defualt as erisComponentsCollector } from './utils/erisComponentsCollector.mjs'
+export { default as removeFormatting } from './utils/removeFormatting.mjs'
+export { default as avgCPUload } from './utils/cpuLoad.mjs';
+export { default as formatTime } from './utils/formatTime.mjs';
+export { default as formatNumber } from './utils/formatNumber.mjs';
+export { default as formatScoreString } from './utils/scoreFormatting.mjs';
+export { default as calculateAccuracy } from './utils/utils/calculateAccuracy.mjs';
+export { default as postBotStats } from './utils/postBotStats.mjs';
+export { default as erisComponentsCollector } from './utils/erisComponentsCollector.mjs'
